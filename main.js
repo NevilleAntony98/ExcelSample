3a00161be50329986302d4b120c096a7179b6592
Neville <nevilleantony98@gmail.com>
2019-02-20 22:01:44 +0530

    updated printData using fetch api

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 function printData(){
-  fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => window.alert(JSON.stringify(json)))
-    
+  var data;
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST', 
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+  .then(response => window.alert(JSON.stringify(response)))
+  .catch(error => console.error('Error:', error));
 }
 
 
